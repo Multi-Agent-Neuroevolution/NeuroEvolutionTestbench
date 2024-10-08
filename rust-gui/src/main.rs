893f4f0b0101b4c6b4611c0a931a6c943fada7c8
CoolHackerMan27 <SALYARDJ@my.erau.edu>
2024-10-08 19:46:21 -0400

    basic UI wireframe created, ready for more development with other funtions and features

--- rust-gui/src/main.rs
+++ rust-gui/src/main.rs
@@ -1,7 +1,7 @@
 use iced::border::width;
-use iced::widget::canvas::fill;
+use iced::widget::canvas::{Canvas, Fill, Frame, Path};
 use iced::widget::{button, canvas, column, pane_grid, row, text, Column, PaneGrid, Row, Text};
-use iced::{mouse, Color, Length, Rectangle, Renderer, Size, Theme};
+use iced::{mouse, Color, Length, Point, Rectangle, Renderer, Size, Theme};
 #[derive(Default, Clone, Copy)]
 struct View {
     counter: i32,
@@ -23,38 +23,46 @@ enum Message {
 }
 
 fn main() {
-    let _ = iced::run("Test App", View::update, View::view);
+    let _ = iced::run(
+        "Multi-agent Neuroevolution Simulator",
+        View::update,
+        View::view,
+    );
 }
 
 impl View {
-    fn view(&self) -> Row<Message> {
+    fn view(&self) -> Column<Message> {
+        //Top Controls
+        let top_controls = row![button("File"), button("Edit"), button("View")].spacing(10);
+
         //SimulationView
         let sim_view = canvas(SimulationView {
             color: Color::from_rgb(1.0, 0.0, 0.0),
         })
-        .width(Length::Fill)
+        .width(Length::FillPortion(3))
         .height(Length::Fill);
         //AgentView
         let agent_view = canvas(AgentView {
-            color: Color::from_rgb(1.0, 1.0, 0.0),
+            color: Color::from_rgb(0.0, 1.0, 0.0),
         })
         .width(Length::Fill)
         .height(Length::Fill);
         //NNView
         let nn_view = canvas(NNView {
-            color: Color::from_rgb(1.0, 1.0, 1.0),
+            color: Color::from_rgb(0.0, 0.0, 1.0),
         })
         .width(Length::Fill)
         .height(Length::Fill);
 
         let agent_nn_col = column!(agent_view, nn_view)
+            .width(Length::FillPortion(1))
+            .height(Length::Fill);
+
+        let content = row!(sim_view, agent_nn_col)
             .width(Length::Fill)
             .height(Length::Fill);
 
-        let container = row!(sim_view, agent_nn_col)
-            .width(Length::Fill)
-            .height(Length::Fill);
-
+        let container = column![top_controls, content];
         container
     }
     fn update(&mut self, message: Message) {
@@ -62,10 +70,14 @@ impl View {
         //     Message::IncrementPressed => self.counter += 1,
         //     Message::DecrementPressed => self.counter -= 1,
         // }
+        //This function will handle updating the simulation, agents, and neural network
+        //It is going to have to use some type of timer so it still updates even if the user is not interacting with the app
     }
 }
 
 impl<Message> canvas::Program<Message> for SimulationView {
+    //This will be in charge of drawing the simulation
+    //The actual simulation will be handled somewhere else. Proabably
     type State = ();
     fn draw(
         &self,
@@ -76,7 +88,7 @@ impl<Message> canvas::Program<Message> for SimulationView {
         _cursor: mouse::Cursor,
     ) -> Vec<canvas::Geometry> {
         let mut frame = canvas::Frame::new(renderer, bounds.size());
-        let rect = canvas::Path::rectangle(frame.center(), frame.size());
+        let rect = canvas::Path::rectangle(Point::new(0.0, 0.0), frame.size());
         frame.fill(&rect, self.color);
         vec![frame.into_geometry()]
     }
@@ -93,7 +105,7 @@ impl<Message> canvas::Program<Message> for AgentView {
         _cursor: mouse::Cursor,
     ) -> Vec<canvas::Geometry> {
         let mut frame = canvas::Frame::new(renderer, bounds.size());
-        let rect = canvas::Path::rectangle(frame.center(), frame.size());
+        let rect = canvas::Path::rectangle(Point::new(0.0, 0.0), frame.size());
         frame.fill(&rect, self.color);
         vec![frame.into_geometry()]
     }
@@ -110,7 +122,7 @@ impl<Message> canvas::Program<Message> for NNView {
         _cursor: mouse::Cursor,
     ) -> Vec<canvas::Geometry> {
         let mut frame = canvas::Frame::new(renderer, bounds.size());
-        let rect = canvas::Path::rectangle(frame.center(), frame.size());
+        let rect = canvas::Path::rectangle(Point::new(0.0, 0.0), frame.size());
         frame.fill(&rect, self.color);
         vec![frame.into_geometry()]
     }
