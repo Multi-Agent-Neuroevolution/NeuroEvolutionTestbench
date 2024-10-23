e681314744a737a999bf44dce8e1718204a0a334
CoolHackerMan27 <jacksonsalyards55@gmail.com>
2024-10-23 14:14:17 -0400

    Added agentview controlls and moved each view to its own struct for better readability and modularity. These controlls/buttons are just placeholders currently.

--- rust-gui/src/main.rs
+++ rust-gui/src/main.rs
@@ -1,18 +1,26 @@
+// This is the main file for the GUI, it will be responsible for creating the GUI and handling user input
+// It is built using the iced crate.
+
 use iced::border::width;
 use iced::widget::canvas::{Canvas, Fill, Frame, Path};
 use iced::widget::{button, canvas, column, pane_grid, row, text, Column, PaneGrid, Row, Text};
 use iced::{mouse, Color, Length, Point, Rectangle, Renderer, Size, Theme};
-mod neural_net;
 #[derive(Default, Clone, Copy)]
 struct View {
     counter: i32,
+    agent_view: AgentView,
+    sim_view: SimulationView,
+    nn_view: NNView,
 }
+#[derive(Default, Clone, Copy)]
 struct SimulationView {
     color: Color,
 }
+#[derive(Default, Clone, Copy)]
 struct AgentView {
     color: Color,
 }
+#[derive(Default, Clone, Copy)]
 struct NNView {
     color: Color,
 }
@@ -32,31 +40,39 @@ fn main() {
 }
 
 impl View {
+    fn new() -> Self {
+        Self {
+            counter: 0,
+            agent_view: AgentView::new(),
+            sim_view: SimulationView::new(),
+            nn_view: NNView::new(),
+        }
+    }
     fn view(&self) -> Column<Message> {
         //Top Controls
-        let top_controls = row![button("File"), button("Edit"), button("View")].spacing(10);
+        let top_controls = row![
+            button("File"),
+            button("Edit"),
+            button("View"),
+            button("<"),
+            text("0x"),
+            button(">")
+        ]
+        .spacing(10);
 
         //SimulationView
-        let sim_view = canvas(SimulationView {
-            color: Color::from_rgb(1.0, 0.0, 0.0),
-        })
-        .width(Length::FillPortion(3))
-        .height(Length::Fill);
+        let sim_view = self
+            .sim_view
+            .view()
+            .width(Length::FillPortion(65))
+            .height(Length::Fill);
         //AgentView
-        let agent_view = canvas(AgentView {
-            color: Color::from_rgb(0.0, 1.0, 0.0),
-        })
-        .width(Length::Fill)
-        .height(Length::Fill);
+        let agent_view = self.agent_view.view();
         //NNView
-        let nn_view = canvas(NNView {
-            color: Color::from_rgb(0.0, 0.0, 1.0),
-        })
-        .width(Length::Fill)
-        .height(Length::Fill);
+        let nn_view = self.nn_view.view();
 
         let agent_nn_col = column!(agent_view, nn_view)
-            .width(Length::FillPortion(1))
+            .width(Length::FillPortion(35))
             .height(Length::Fill);
 
         let content = row!(sim_view, agent_nn_col)
@@ -73,6 +89,69 @@ impl View {
         // }
         //This function will handle updating the simulation, agents, and neural network
         //It is going to have to use some type of timer so it still updates even if the user is not interacting with the app
+        //It will also need to interface with a websocket to get the data from the simulation, so it is going to need to be async or something
+    }
+}
+
+impl AgentView {
+    pub fn new() -> Self {
+        Self {
+            color: Color::from_rgb(0.0, 1.0, 0.0),
+        }
+    }
+    fn view(&self) -> Column<Message> {
+        let agent_view = canvas(AgentView {
+            color: Color::from_rgb(0.0, 1.0, 0.0),
+        })
+        .width(Length::Fill)
+        .height(Length::Fill);
+        let container = column![agent_view, self.controlls()];
+        container
+    }
+    fn controlls(&self) -> Row<Message> {
+        let controls = row![
+            button("Add Agent"),
+            button("Remove Agent"),
+            button("Pause"),
+            button("Play"),
+            button("Step"),
+            button("Reset")
+        ];
+        controls
+    }
+}
+
+impl NNView {
+    pub fn new() -> Self {
+        Self {
+            color: Color::from_rgb(0.0, 0.0, 1.0),
+        }
+    }
+    fn view(&self) -> Column<Message> {
+        let nn_view = canvas(NNView {
+            color: Color::from_rgb(0.0, 0.0, 1.0),
+        })
+        .width(Length::Fill)
+        .height(Length::Fill);
+        let container = column![nn_view];
+        container
+    }
+}
+
+impl SimulationView {
+    pub fn new() -> Self {
+        Self {
+            color: Color::from_rgb(1.0, 0.0, 0.0),
+        }
+    }
+    fn view(&self) -> Column<Message> {
+        let sim_view = canvas(SimulationView {
+            color: Color::from_rgb(1.0, 0.0, 0.0),
+        })
+        .width(Length::Fill)
+        .height(Length::Fill);
+        let container = column![sim_view];
+        container
     }
 }
 
