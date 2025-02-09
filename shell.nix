882dbd84f292d0e51098a933575a53ad3982f65c
SpoonFll <baker.jackson@rocketmail.com>
2025-02-09 16:19:22 -0500

    generated python api, made server example

--- shell.nix
+++ shell.nix
@@ -7,9 +7,10 @@ mkShell {
     grpc-tools
     poetry
     xorg.libX11
+    gnumake
   ];
   LD_LIBRARY_PATH="$LD_LIBRARY_PATH:${
       with pkgs;
-      lib.makeLibraryPath [ libGL xorg.libX11 xorg.libXi xorg.libXcursor libxkbcommon ]
+      lib.makeLibraryPath [ libGL xorg.libX11 xorg.libXi xorg.libXcursor libxkbcommon stdenv.cc.cc]
     }";
 }
