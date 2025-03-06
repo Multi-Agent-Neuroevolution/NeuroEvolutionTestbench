1b4cdc55681072fe0e0c6e4a6186c494fac13d6f
SpoonFll <baker.jackson@rocketmail.com>
2025-03-06 15:09:26 -0500

    working server

--- src/simulation/server.py
+++ src/simulation/server.py
@@ -12,13 +12,15 @@ import logs
 from environment import Environment
 from agents import Predator, Prey
 from evolution_utils import breed_and_mutate
-from .generated import comms_pb2, comms_pb2_grpc
+#from .generated import comms_pb2, comms_pb2_grpc
 from messenger import messageChannel
+import comms_pb2
+import comms_pb2_grpc
 
 # Initialize logger
 logger = logging.getLogger(__name__)
 
-class CommunincationService(comms_pb2_grpc.CommunicationServicer):
+class CommunicationService(comms_pb2_grpc.CommunicationServicer):
     def FetchEnvironmentStream(self, request, context):
         while messageChannel.empty == False:
             data = messageChannel.get()
